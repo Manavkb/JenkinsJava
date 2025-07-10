637b4ac3074f6a3feb037d58f69463fbccb0dcbe
Manavkb <manavkb@gmail.com>
2025-07-10 19:01:54 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main
+{
+    public static void main(String [] args)
+    {
+        System.out.println("This is java prg");
+        System.out.println("Output from java prg");
+        System.out.println(new Date());
+    }
+}

d04943e0954b8c28684847000192e83ff4500c9d
Manavkb <manavkb@gmail.com>
2025-07-10 20:29:57 +0530

    HCL Commit

--- Main.java
+++ Main.java
@@ -9,5 +9,6 @@ public class Main
         System.out.println(new Date());
         System.out.println("Hello");
         System.out.println("Added one more file");
+        System.out.println("HCL file added");
     }
 }
